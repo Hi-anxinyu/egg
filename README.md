b3cf44b8bcae4d437926c31113c5a8b9888e0f3e
Hi-anxinyu <1778120064@qq.com>
2021-12-21 15:19:31 +0800

    '目录结构'

--- README.md
+++ README.md
@@ -31,3 +31,20 @@ $ npm stop
 
 
 [egg]: https://eggjs.org
+
+### 目录结构
+
+- app 项目主要开发目录
+    - controller 控制器目录
+    - router.js  项目的路由配置文件
+
+- config 项目的配置目录
+    - config.default.js 项目配置文件
+    - plugin.js         插件配置文件
+
+- test 项目单元测试使用的文件
+
+### 脚本命令
+ - start 生产环境使用，也就是开发完成部署到线上的时候，拥有守护进程，终端不会被占用，一般不会掉线，修改代码之后需要重启服务。
+ - dev   开发环境使用，修改代码不需要重启服务。
+ - stop  用来停止服务。
--- app/controller/home.js
+++ app/controller/home.js
@@ -1,11 +1,15 @@
-'use strict';
+"use strict";
 
-const Controller = require('egg').Controller;
+const Controller = require("egg").Controller;
 
 class HomeController extends Controller {
   async index() {
     const { ctx } = this;
-    ctx.body = 'hi, egg';
+    ctx.body = "你好，鸡蛋";
+  }
+  async shanks() {
+    const { ctx } = this;
+    ctx.body = '<h1>你好，香克斯</h1>'
   }
 }
 
